5b20f3b5fd46cb7878455fe8bf75e5e82d668874
taeguen park <rmptgw@gmail.com>
2023-01-09 09:38:33 +0000

    complete Problem No.3

--- project_euler/Problem3.R
+++ project_euler/Problem3.R
@@ -1,4 +1,27 @@
-x <- 600851475143
+# 문제 사이트 : https://euler.synap.co.kr/problem=3
+# 사용언어 : R
+# 레포지토리 : git
 
-asd 
+x <- 600851475143;
+# x <- 13195;
 
+# x <- 10;
+
+print(x)
+
+i <- 2
+fin <- c()
+
+while(i <= x){
+	if( x %% i == 0 ){
+		cat("i : ", i, "\n")
+		x <- x / i
+		fin <- c(fin, i)
+		fin <- unique(fin)
+		fin <- sort(fin)
+		next
+	} 
+	i <- i + 1	
+}
+cat("######\n")
+print(fin)
