08a1c264b7d4d6340fdffaf67ec25e203dc3a0d0
TG <rmptgw@gmail.com>
2023-08-03 18:36:58 +0900

    중간 저장

--- project_euler/Problem005.py
+++ project_euler/Problem005.py
@@ -1,14 +1,14 @@
+# 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수
+# 1 ~ 10 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 2520입니다.
+# 그러면 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?
 result = 0
-i = 1
-while(True):
-    conn = True
-    for k in range(1,21):
-        if( i % k > 0 ):
-            conn = False
-            break
-        
-    if( conn == True ):
-        break
-    i += 1
-    
-print(result)
+num = []
+for k in range(2,21):
+    for i in range(1,k+1):
+        if(k % i == 0):
+            num.append(i)
+            continue   
+
+num = set(num)
+
+print(num)
