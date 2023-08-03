19e1db804898bfcd5e95fa9dcdc912fea15c2109
TG <rmptgw@gmail.com>
2023-08-03 17:35:21 +0900

    완료

--- project_euler/Problem004.py
+++ project_euler/Problem004.py
@@ -1,22 +1,33 @@
-x = 2
+x = 3
 result = 0
-for i in range(x*2, 0, -1):
-    num = str(i)
-    num = list(num)
-    
-    
-    for j in range(0, x):
-        a = num[j]
-        b = num[-j]
-        
-        print("a : " + a + ", b : " + b)
-        if(a!= b):
-            conn = 'F'
-        else :
-            conn = 'T'
-    
-    if(conn == 'T'):
-        result = i
-        break;
-        
+a = 0
+b = 0
+nn = 10 ** x - 1
+# print(nn)
+
+for i in range(nn, 0, -1):
+    for j in range(nn, 0, -1):
+        num = i * j
+        num = str(num)
+        num = list(num)
+
+        conn = 'T'
+        for k in range(0, x):
+            a = num[k]
+            b = num[-k-1]
+            
+            print("num = ", num, "k : " + str(k) + " a : " + a + ", b : " + b)
+            print('conn : ' + conn, "i : ", i, "j : ", j)
+            if(a!= b):
+                conn = 'F'
+                break
+
+        if(conn == 'T' ):
+            if(result < (i * j)):
+                result = i * j
+                break
+    if(conn == 'T' ):
+        if(i < (nn/2)):
+            break
+
 print(result)
