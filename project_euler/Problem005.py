13605420083f3a23220bc548e97d12add62c5677
TG <rmptgw@gmail.com>
2023-08-04 17:49:12 +0900

    소인수분해 부분 작성 중

--- project_euler/Problem005.py
+++ project_euler/Problem005.py
@@ -4,15 +4,27 @@
 import numpy as np
 result = 0
 num = []
+prime_factor = [[1][0]]
+# Solve : 2 ~ 20 까지 소인수분해 후 최대공약수 구하기
 for k in range(2,21):
+    # 각 수에서 약수 담을 list
     div = []
-    for i in range(1,k+1):
+    
+    # 약수 구하기
+    for i in range(1,k + 1):
         if(k % i == 0 & i != 1):
             div.append(i)
             continue
     
+    # 약수 출력
+    print(div)
+
     if(len(div) == 1):
+        # 소수이면 num 추가
         num.append(k)
     else:
+        # 소수가 아니면 소인수분해
         for i in div:
-            a
+            while(True):
+                if(k%i==0):
+                     
